aa25f7006dfa74b5681ced74be590500de2fe9fe
unknown <mattcmej@gmail.com>
2018-05-01 18:10:38 -0700

    Install request node package.

--- liri.js
+++ liri.js
@@ -43,13 +43,12 @@ function showTweets() {
             // If you want to check the various properties of the object, uncomment the line below:
             // console.log(tweet[i]);
 
-            console.log("");
-            console.log("Poster: " + tweet[i].user.name);
-            console.log("Post: " + tweet[i].text);
-            console.log("Date Created: " + tweet[i].user.created_at);
-            console.log("");
-            console.log("----------------------------------");
-            // console.log("");
+            console.log("");                                                            // spacing 
+            console.log("Poster: " + tweet[i].user.name);                               // poster of the tweet
+            console.log("Post: " + tweet[i].text);                                      // the post itself
+            console.log("Date Created: " + tweet[i].user.created_at);                   // date the post was created
+            console.log("");                                                            // spacing
+            console.log("----------------------------------");                          // separator
         }
         // console.log(tweet);
         // JSON.stringify(console.log(tweet), null, 2);
@@ -61,22 +60,23 @@ function showTweets() {
 function spotifySong() {
 
     var spotifySearch = new spotify(keys.spotify);
+
+    // empty string to hold the user's song entry
     var song = "";
+
     // var test = "this is a test";
     // console.log(test);
     // console.log(processArray[3]);
     // console.log(processArray[4]); 
     // console.log(test + " " + processArray[3]);  
 
+    // this loop will loop through the user's entered text in the command line and 
+    // add all that text to the "song" string variable.
     for (i = 3; i < processArray.length; i++) {
         song = song + " " + processArray[i];
-        // console.log(song);
     }
 
-    // console.log(song);
-
-
-
+    // spotify method to retrieve song data
     spotifySearch.search({
         type: "track",
         query: song
